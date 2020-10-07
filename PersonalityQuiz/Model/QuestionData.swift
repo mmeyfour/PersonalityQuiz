0e29654d43ac2f089015715dbcbfa1a11fc37a7b
Mersad Meyfour Asadi <Mmeyfour@iCloud.com>
2020-10-07 18:01:29 +0200

    question completed !!

--- PersonalityQuiz/Model/QuestionData.swift
+++ PersonalityQuiz/Model/QuestionData.swift
@@ -35,21 +35,21 @@ var questions = [
         ]
     ),
     Question(
-        text: "Which Activities Do You Prefer ?",
+        text: "Which Activities Do You enjoy ?",
         answers: [
-            Answer(text: "Swimming", type: .dog),
-            Answer(text: "Fish", type: .cat),
-            Answer(text: "Carrots", type: .rabbit),
-            Answer(text: "Corn", type: .turtle)
+            Answer(text: "Swimming", type: .turtle),
+            Answer(text: "sleeping", type: .cat),
+            Answer(text: "Cudding", type: .rabbit),
+            Answer(text: "Eating", type: .dog)
         ]
     ),
     Question(
-        text: "How Much Do You ?",
+        text: "How Much Do You enjoy car rides?",
         answers: [
-            Answer(text: "Steak", type: .dog),
-            Answer(text: "Fish", type: .cat),
-            Answer(text: "Carrots", type: .rabbit),
-            Answer(text: "Corn", type: .turtle)
+            Answer(text: "I dislike them", type: .cat),
+            Answer(text: "I get a little nervous", type: .rabbit),
+            Answer(text: "Ibearly notice them", type: .turtle),
+            Answer(text: "I love Them", type: .dog)
         ]
     )
 ]

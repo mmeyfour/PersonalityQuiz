ca8dacaa59cc386911f1e51942a4fadd736e65e1
Mersad Meyfour Asadi <Mmeyfour@iCloud.com>
2020-10-07 17:57:34 +0200

    navigationBar created

--- PersonalityQuiz/Controller/QuestionViewController.swift
+++ PersonalityQuiz/Controller/QuestionViewController.swift
@@ -8,22 +8,30 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
-
+    
+    // MARK: Outlets
+    @IBOutlet weak var questionProgressView: UIProgressView!
+    @IBOutlet weak var QuestionLabel: UILabel!
+    @IBOutlet weak var answerOneButton: UIButton!
+    @IBOutlet weak var answerTwoButton: UIButton!
+    @IBOutlet weak var answerThreeButton: UIButton!
+    @IBOutlet weak var answerFourButton: UIButton!
+    
+    // MARK: State
+    var question: Question?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        question = questions[2]
+        updateUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateUI(){
+        QuestionLabel.text = question?.text
+        answerOneButton.setTitle(question?.answers[0].text, for: .normal)
+        answerTwoButton.setTitle(question?.answers[1].text, for: .normal)
+        answerThreeButton.setTitle(question?.answers[2].text, for: .normal)
+        answerFourButton.setTitle(question?.answers[3].text, for: .normal)
     }
-    */
-
+    
 }
--- PersonalityQuiz/Model/QuestionData.swift
+++ PersonalityQuiz/Model/QuestionData.swift
@@ -23,3 +23,33 @@ enum Animal {
     case rabbit
     case turtle
 }
+
+var questions = [
+    Question(
+        text: "What is your favorite food ?",
+        answers: [
+            Answer(text: "Steak", type: .dog),
+            Answer(text: "Fish", type: .cat),
+            Answer(text: "Carrots", type: .rabbit),
+            Answer(text: "Corn", type: .turtle)
+        ]
+    ),
+    Question(
+        text: "Which Activities Do You Prefer ?",
+        answers: [
+            Answer(text: "Swimming", type: .dog),
+            Answer(text: "Fish", type: .cat),
+            Answer(text: "Carrots", type: .rabbit),
+            Answer(text: "Corn", type: .turtle)
+        ]
+    ),
+    Question(
+        text: "How Much Do You ?",
+        answers: [
+            Answer(text: "Steak", type: .dog),
+            Answer(text: "Fish", type: .cat),
+            Answer(text: "Carrots", type: .rabbit),
+            Answer(text: "Corn", type: .turtle)
+        ]
+    )
+]

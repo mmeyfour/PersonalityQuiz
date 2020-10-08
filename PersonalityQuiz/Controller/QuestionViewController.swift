4b56d4cfc88f901be2e21c2dd8a639d33c8283aa
Mersad Meyfour Asadi <Mmeyfour@iCloud.com>
2020-10-08 10:28:45 +0200

    resultViewController created

--- PersonalityQuiz/Controller/QuestionViewController.swift
+++ PersonalityQuiz/Controller/QuestionViewController.swift
@@ -19,7 +19,7 @@ class QuestionViewController: UIViewController {
     
     // MARK: State
     var question: Question?
-    var counter = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,22 +30,22 @@ class QuestionViewController: UIViewController {
     // MARK: Helpers
     func updateUI(){
         
-        question = questions[counter]
-        questionLabel.text = question?.text
-        answerOneButton.setTitle(question?.answers[0].text, for: .normal)
-        answerTwoButton.setTitle(question?.answers[1].text, for: .normal)
-        answerThreeButton.setTitle(question?.answers[2].text, for: .normal)
-        answerFourButton.setTitle(question?.answers[3].text, for: .normal)
-        if counter == 0 {
-            counter = 1
-            questionProgressView.progress = 0
-        } else if counter == 1{
-            counter = 2
-            questionProgressView.progress = 0.5
-        } else {
-            counter = 0
-            questionProgressView.progress = 1
+//        //Method One: Optional chaining
+//        questionLabel.text = question?.text
+//        answerOneButton.setTitle(question?.answers[0].text, for: .normal)
+//        answerTwoButton.setTitle(question?.answers[1].text, for: .normal)
+//        answerThreeButton.setTitle(question?.answers[2].text, for: .normal)
+//        answerFourButton.setTitle(question?.answers[3].text, for: .normal)
+        
+        // Method Two:
+        guard let question = question else {
+            return
         }
+        questionLabel.text = question.text
+        answerOneButton.setTitle(question.answers[0].text, for: .normal)
+        answerTwoButton.setTitle(question.answers[1].text, for: .normal)
+        answerThreeButton.setTitle(question.answers[2].text, for: .normal)
+        answerFourButton.setTitle(question.answers[3].text, for: .normal)
     }
     @IBAction func didTappedFirstButton(_ sender: UIButton) {
         updateUI()
